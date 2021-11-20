55f140ae5d4e2c810013329576a2d2bfca28c76b
jb2328 <jb2328@cam.ac.uk>
2021-11-20 17:31:42 +0000

    ex1

--- part2/python/potential_field.py
+++ part2/python/potential_field.py
@@ -19,7 +19,34 @@ def get_velocity_to_reach_goal(position, goal_position):
   v = np.zeros(2, dtype=np.float32)
   # MISSING: Compute the velocity field needed to reach goal_position
   # assuming that there are no obstacles.
+ 
+  #get current and goal positions
+  current=position
 
+  #the goal position is always 1.5 and 1.5
+  goal=goal_position
+
+  ## get the x values for current and goal positions
+  x1=current[0]
+  x2=goal[0]
+
+  ##get the y value sfor current and goal positions
+  y1=current[1]
+  y2=goal[1]
+  
+  #get the distance between current position and the goal
+  #(x2-x1)^2 + (y2-y1)^2
+  dist=np.sqrt((x2-x1)**2+(y2-y1)**2)
+
+  #subtract the current vector field values from the distance
+  v[0]=dist-x1
+  v[1]=dist-y1
+
+  #OPTIONAL: get the max vector's magnitude
+  magnitude=np.linalg.norm(v)
+  #if(magnitude>9):
+  #  print('magnitude:', magnitude)
+  
   return v
 
 
@@ -32,7 +59,50 @@ def get_velocity_to_avoid_obstacles(position, obstacle_positions, obstacle_radii
   # speeds that are larger than max_speed for each obstacle. Both obstacle_positions
   # and obstacle_radii are lists.
 
-  return v
+ #get current and goal positions
+  current=position
+
+  #the obstacle position [single array entry?]
+  ##for now only assume there is one obstacle
+  obstacles=obstacle_positions
+
+  ## get the x values for current and obstacle positions
+  x1=current[0]
+
+  ##get the y value sfor current and obstacle positions
+  y1=current[1]
+
+  dists_to_obstacles=[]
+
+  iterator=0
+  for obstacle in obstacles:
+      #get the distance between current position and the obstacle
+      #(x2-x1)^2 + (y2-y1)^2
+      x2_obs=obstacle[0]
+      y2_obs=obstacle[1]
+
+      dist_to_obst=np.sqrt((x2_obs-x1)**2+(y2_obs-y1)**2)-obstacle_radii[iterator]
+      dists_to_obstacles.append(dist_to_obst)
+      iterator+=1
+        
+# 
+  # ##get the distance from the current position and the obstacle
+  # print('obs_positions',obstacle_positions)
+  # print('obs_radii',obstacle_radii)
+
+  #obstacle radii multiplier
+  #iterator2=0
+  for dists in dists_to_obstacles:
+      v[0]=((dists-x1)*-1)
+      v[1]=((dists-y1)*-1)
+
+      magnitude=np.linalg.norm(v)
+
+      #print(v,magnitude)
+  print(v,cap(v,MAX_SPEED))
+
+      ##make sure v isn't more than max speed
+  return cap(v, 0.5)
 
 
 def normalize(v):
